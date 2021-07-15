d7f04897e5660b7290878fb68f57dc842b44d7e8
Tiago Seabra <tlgs@users.noreply.github.com>
2021-07-16 00:57:16 +0100

    refactor: simplify looping

--- 02-slices.go
+++ 02-slices.go
@@ -4,12 +4,11 @@ import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
 	p := make([][]uint8, dy)
-	for y := range p {
-		q := make([]uint8, dx)
-		for x := range q {
-			q[x] = uint8(x ^ y)
+	for y := 0; y < dy; y++ {
+		p[y] = make([]uint8, dx)
+		for x := 0; x < dx; x++ {
+			p[y][x] = uint8(x ^ y)
 		}
-		p[y] = q
 	}
 
 	return p
