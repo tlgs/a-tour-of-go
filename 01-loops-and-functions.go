cfcdad3f5c69e3f46b1613cc8694d1fbf4b59b27
Tiago Seabra <tlgs@users.noreply.github.com>
2021-07-16 00:49:48 +0100

    refactor: use math.Abs

--- 01-loops-and-functions.go
+++ 01-loops-and-functions.go
@@ -1,23 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func Sqrt(x float64) float64 {
 	z := 1.0
-	for epsilon := 1.0; epsilon > 1e-10; {
-		diff := (z*z - x) / (2 * z)
-		z -= diff
-
-		if diff > 0 {
-			epsilon = diff
-		} else {
-			epsilon = -diff
-		}
+	for epsilon := 1.0; math.Abs(epsilon) > 1e-10; {
+		epsilon = (z*z - x) / (2 * z)
+		z -= epsilon
 	}
 
 	return z
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	fmt.Println("Sqrt(2)     :", Sqrt(2))
+	fmt.Println("math.Sqrt(2):", math.Sqrt(2))
 }
